275ae16f3fb379348c6fa875cdc87cec0a62ce42
Shadowrunner11 <matematicadivina1@gmail.com>
2022-10-19 07:43:47 -0500

    :pencil: added animations

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,20 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import AnimatedText from './components/animatedText'
+import AnimatedElipsis from './components/AnimatedElipsis'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-     <AnimatedText />
+     <AnimatedText data={[
+  'Krowdy 1',
+  'Krowdy 2',
+  'Krowdy 3',
+  'Krowdy 4'
+]}/>
+    <AnimatedElipsis />
     </div>
   )
 }
